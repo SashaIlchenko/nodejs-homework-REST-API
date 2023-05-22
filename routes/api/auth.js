604e64b377dbbafd058bb36f86e09889f8d8d45b
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-05-22 09:49:10 +0300

    create user model

---
+++ routes/api/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+
+const { validateBody } = require("../../middlewares");
+const { schemas } = require('../../models/user');
+const router = express.Router();
+
+
+module.exports = router;
