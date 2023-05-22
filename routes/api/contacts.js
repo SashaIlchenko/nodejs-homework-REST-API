1cb02d60704fed33e2c2d05509c79bc472a1434d
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-05-22 11:41:37 +0300

    create middleware for validation token

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,20 @@ const express = require('express');
 
 const contactController = require('../../controllers/contacts-controllers');
 
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require('../../models/contacts');
 const router = express.Router();
 
-router.get('/', contactController.getAllContacts);
+router.get('/', authenticate, contactController.getAllContacts);
 
-router.get('/:contactId', contactController.getContactById, isValidId);
+router.get('/:contactId', authenticate, contactController.getContactById, isValidId);
 
-router.post('/', validateBody(schemas.addSchema), contactController.addContact);
+router.post('/', authenticate, validateBody(schemas.addSchema), contactController.addContact);
 
-router.patch('/:contactId/favourite', validateBody(schemas.updateFavoriteSchema), contactController.updateStatusContact, isValidId);
+router.patch('/:contactId/favourite', authenticate, validateBody(schemas.updateFavoriteSchema), contactController.updateStatusContact, isValidId);
 
-router.delete('/:contactId', contactController.deleteContactById, isValidId);
+router.delete('/:contactId', authenticate, contactController.deleteContactById, isValidId);
 
-router.put('/:contactId', validateBody(schemas.addSchema), contactController.updateContactById, isValidId);
+router.put('/:contactId', authenticate, validateBody(schemas.addSchema), contactController.updateContactById, isValidId);
 
 module.exports = router;
