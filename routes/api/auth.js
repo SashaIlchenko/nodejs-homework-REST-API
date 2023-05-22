cb339b2cc8e42ef79cdd3c15130201ade7cfba03
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-05-22 10:02:10 +0300

    add register router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
-
 const { validateBody } = require("../../middlewares");
 const { schemas } = require('../../models/user');
+const userController = require('../../controllers/user-controllers')
 const router = express.Router();
-
+router.post('/register', validateBody(schemas.registerSchema), userController.register)
 
 module.exports = router;
